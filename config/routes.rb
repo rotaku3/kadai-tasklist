d3edfca4de0cded5f4bd09bf5083d95fb2ae5957
takuro.katsube <takuroktb@gmail.com>
2021-07-08 14:30:58 +0000

    fix5

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
     root to: 'tasks#index'
     
     get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
   
     get 'signup', to: 'users#new'
     resources :tasks
