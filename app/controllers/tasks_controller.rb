ab92b0d6eef7f1de88a078be5cd3bb98edd95509
takuro.katsube <takuroktb@gmail.com>
2021-07-08 09:24:47 +0000

    fix2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+ 
   before_action :require_user_logged_in
   before_action :correct_user, only: [:show,:edit,:update,:destroy]
 
@@ -64,9 +64,7 @@ class TasksController < ApplicationController
   
   private
   
-  def set_task
-    @task = Task.find(params[:id])
-  end
+  
 
   # Strong Parameter
   def task_params
